c43b92511a02fdcd0749af69f366b64ac7ac24ce
Bozhidar Nemski <bojonemski@gmail.com>
2022-04-06 19:11:50 +0300

    Added ability to scroll to the last message

--- client/src/components/chat/MessagesZone.tsx
+++ client/src/components/chat/MessagesZone.tsx
@@ -1,6 +1,9 @@
+import { useRef, useEffect } from 'react';
+
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Message from 'components/chat/Message';
+
 import { useAppSelector } from 'app/hooks';
 
 type Props = {
@@ -8,9 +11,22 @@ type Props = {
 };
 
 const MessagesZone = ({ roomId }: Props) => {
+    const messageBoxRef = useRef<HTMLDivElement>(null);
     const messages = useAppSelector((state) =>
         state.chats.chats.find((chat) => chat.roomId === roomId)
     )?.messages;
+
+    useEffect(() => {
+        if (messageBoxRef && messageBoxRef.current) {
+            const { scrollHeight, clientHeight } = messageBoxRef.current;
+            messageBoxRef.current.scrollTo({
+                left: 0,
+                top: scrollHeight - clientHeight,
+                behavior: 'smooth',
+            });
+        }
+    }, [messages]);
+
     return roomId ? (
         <>
             <Box
@@ -22,6 +38,7 @@ const MessagesZone = ({ roomId }: Props) => {
                     overflowY: 'scroll',
                     pt: 2,
                 }}
+                ref={messageBoxRef}
             >
                 {messages?.map((message, index) => (
                     <Message
